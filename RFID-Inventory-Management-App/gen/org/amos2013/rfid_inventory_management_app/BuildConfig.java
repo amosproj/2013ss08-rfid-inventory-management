0df21bebfeeb6376f348a5502835b383800c9358
a-singer <a-singer@users.sf.net>
2013-04-26 22:19:31 +0200

    added the Android App, which is showing the project's website

---
+++ RFID-Inventory-Management-App/gen/org/amos2013/rfid_inventory_management_app/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.amos2013.rfid_inventory_management_app;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ RFID-Inventory-Management-App/src/org/amos2013/rfid_inventory_management_app/MainActivity.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2013 by 
+ * AMOS 2013 Group 8: RFID Inventory Management (Elektrobit)
+ *
+ * POs:
+ *  Andreas Lutz
+ *  Jana Riechert
+ *  Kerstin Stern
+ * 
+ * SDs:
+ *  Andreas Singer
+ *  Liping Wang
+ *  David Lehmeier
+ *
+ * This file is part of the RFID Inventory Management application.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+package org.amos2013.rfid_inventory_management_app;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		WebView proj8WebView = (WebView) findViewById(R.id.webView);
+		WebSettings webSettings = proj8WebView.getSettings();
+		webSettings.setJavaScriptEnabled(true);
+	    webSettings.setSupportZoom(true);
+	    webSettings.setBuiltInZoomControls(true);
+	    webSettings.setDefaultZoom(WebSettings.ZoomDensity.FAR);
+	    webSettings.setLoadWithOverviewMode(true);
+	    webSettings.setUseWideViewPort(true);
+	    
+	    // clicked links will be opened inside the WebView (default: default browser)
+	    proj8WebView.setWebViewClient(new WebViewClient());
+	    
+	    proj8WebView.loadUrl("http://proj8.ss13.osramos.de/");
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
