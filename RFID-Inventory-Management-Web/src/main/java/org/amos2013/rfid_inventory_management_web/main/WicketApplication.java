f556dd0e78132e325c8ebf8f9fdd405df9802e2a
a-singer <a-singer@users.sf.net>
2013-05-23 07:22:07 +0200

    # feature 37: - desktop site is now root, app site is "/app" - added a link from the desktop site to the app site

--- RFID-Inventory-Management-Web/src/main/java/org/amos2013/rfid_inventory_management_web/main/WicketApplication.java
+++ RFID-Inventory-Management-Web/src/main/java/org/amos2013/rfid_inventory_management_web/main/WicketApplication.java
@@ -71,9 +71,9 @@ public class WicketApplication extends WebApplication
 	{
 		super.init();
 		// mount homepage and pages without the PageComponentInfo being displayed
-		mount(new HidePageComponentInfoMounter("/main", ListPage.class));
+		mount(new HidePageComponentInfoMounter("/app", AppPage.class));
 		mount(new HidePageComponentInfoMounter("/admin", AdminPage.class));
-		mount(new HidePageComponentInfoMounter("/main/search", SearchPage.class));
-		mount(new HideHomePageComponentInfoMounter(AppPage.class));
+		mount(new HidePageComponentInfoMounter("/search", SearchPage.class));
+		mount(new HideHomePageComponentInfoMounter(ListPage.class));
 	}
 }
