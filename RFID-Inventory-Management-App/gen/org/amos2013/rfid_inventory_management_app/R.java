53d3111545e1c54accd80c2eaa24007a2b7c78fc
a-singer <a-singer@users.sf.net>
2013-06-08 19:31:33 +0200

    #feature 55 - created native app: which is displaying the (input) fields dynamically. - after stopping the scan process, the result list gets clickable. - long press on a record will display more info to this tag. - after selecting records, the location, room and employee drop down menu will be shown. - TODO: filling room and employee drop down choices is not working yet! after choosing a location, a NoClassDefFoundError will be shown. there is something wrong with including our web project classes...

--- RFID-Inventory-Management-App/gen/org/amos2013/rfid_inventory_management_app/R.java
+++ RFID-Inventory-Management-App/gen/org/amos2013/rfid_inventory_management_app/R.java
@@ -21,14 +21,31 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int border_ui=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int webView=0x7f080000;
+        public static final int action_settings=0x7f080010;
+        public static final int buttonSave=0x7f08000f;
+        public static final int buttonStartStopScanning=0x7f080002;
+        public static final int linearLayoutEmployee=0x7f08000c;
+        public static final int linearLayoutInput=0x7f080005;
+        public static final int linearLayoutLocation=0x7f080006;
+        public static final int linearLayoutMain=0x7f080000;
+        public static final int linearLayoutRoom=0x7f080009;
+        public static final int listViewScannedTags=0x7f080004;
+        public static final int spinnerEmployee=0x7f08000e;
+        public static final int spinnerLocation=0x7f080008;
+        public static final int spinnerRoom=0x7f08000b;
+        public static final int textViewEmployee=0x7f08000d;
+        public static final int textViewLocation=0x7f080007;
+        public static final int textViewRoom=0x7f08000a;
+        public static final int textViewScannedTags=0x7f080003;
+        public static final int textViewStatus=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int scanned_tags_list_element=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- RFID-Inventory-Management-App/src/org/amos2013/rfid_inventory_management_app/MainActivity.java
+++ RFID-Inventory-Management-App/src/org/amos2013/rfid_inventory_management_app/MainActivity.java
@@ -31,41 +31,244 @@
 
 package org.amos2013.rfid_inventory_management_app;
 
-import android.os.Bundle;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.amos2013.rfid_inventory_management_web.database.EmployeeDatabaseHandler;
+import org.amos2013.rfid_inventory_management_web.database.RoomDatabaseHandler;
+
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.SparseBooleanArray;
+import android.view.Gravity;
 import android.view.Menu;
-import android.view.Window;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * The Class MainActivity.
  */
 public class MainActivity extends Activity
 {
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		WebView proj8WebView = (WebView) findViewById(R.id.webView);
-		WebSettings webSettings = proj8WebView.getSettings();
-		webSettings.setJavaScriptEnabled(false);
-	    webSettings.setSupportZoom(true);
-	    webSettings.setBuiltInZoomControls(true);
-	    webSettings.setDefaultZoom(WebSettings.ZoomDensity.CLOSE);
-	    webSettings.setLoadWithOverviewMode(false);
-	    webSettings.setUseWideViewPort(false);
-	    	    
-	    // clicked links will be opened inside the WebView (default: default browser)
-	    proj8WebView.setWebViewClient(new WebViewClient());
-	    
-	    proj8WebView.loadUrl("http://proj8.ss13.osramos.de/app/");
-	}
+		final Button saveButton = (Button) findViewById(R.id.buttonSave);
+		
+		// set up spinners
+		final Spinner spinnerLocation = (Spinner) findViewById(R.id.spinnerLocation);
+		final Spinner spinnerRoom = (Spinner) findViewById(R.id.spinnerRoom);
+		final Spinner spinnerEmployee = (Spinner) findViewById(R.id.spinnerEmployee);
+		
+		String[] locationList = new String[] { "Please select", "Tennenlohe (DE)", "Betholl (US)"};
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, locationList);
+	    spinnerLocation.setAdapter(adapter);
+	    spinnerLocation.setEnabled(false);
+		spinnerLocation.setOnItemSelectedListener(new OnItemSelectedListener() 
+		{
+			@Override
+			public void onItemSelected(AdapterView<?> arg0, View parent, int position, long id)
+			{
+				// if "Please select" is selected, don't enable the others
+				if (position == 0)
+				{
+					spinnerRoom.setEnabled(false);
+					spinnerEmployee.setEnabled(false);
+				}
+				else
+				{
+					spinnerRoom.setEnabled(true);
+					spinnerEmployee.setEnabled(true);
+					
+					//fill room dropdown menu choices
+					String selectedLocation = (String) spinnerLocation.getItemAtPosition(position);
+					
+					List<String> roomChoicesList = null;
+					TextView textViewStatus = (TextView) findViewById(R.id.textViewStatus);
+					try
+					{
+						roomChoicesList = RoomDatabaseHandler.getRecordsFromDatabaseByLocation(selectedLocation);
+					} 
+					catch (Exception e)
+					{
+						textViewStatus.setText(e.getMessage());
+					}
+					
+					roomChoicesList.add(0, "Please select");
+					spinnerRoom.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item, roomChoicesList));
+					
+					// fill employee drop down choices
+					List<String> employeeChoicesList = null;
+					try
+					{
+						employeeChoicesList = EmployeeDatabaseHandler.getRecordsFromDatabaseByLocation(selectedLocation);
+					} 
+					catch (SQLException e)
+					{
+						textViewStatus.setText(e.getMessage());
+					}
+					
+					employeeChoicesList.add("please select");
+					spinnerEmployee.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item, employeeChoicesList));
+				}
+			}
 
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0)
+			{
+				
+			}
+		});	
+		
+		// if room and employee spinners have a selected item, enable the save button
+		spinnerRoom.setOnItemSelectedListener(new OnItemSelectedListener()
+		{
+			@Override
+			public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id)
+			{
+				if (position != 0 && spinnerEmployee.getSelectedItemId() != 0)
+				{
+					saveButton.setEnabled(true);
+				}
+			}
+			
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0)
+			{
+				
+			}
+		});
+		
+		spinnerEmployee.setOnItemSelectedListener(new OnItemSelectedListener()
+		{
+			@Override
+			public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id)
+			{
+				if (position != 0 && spinnerRoom.getSelectedItemId() != 0)
+				{
+					saveButton.setEnabled(true);
+				}
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0)
+			{
+				
+			}
+		});
+		
+		// set up listview, where the scanned items are listed
+		final ListView listViewScannedTags = (ListView) findViewById(R.id.listViewScannedTags);
+		listViewScannedTags.setOnItemClickListener(new OnItemClickListener()
+		{
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int position, long id)
+			{
+				LinearLayout inputLayout = (LinearLayout) findViewById(R.id.linearLayoutInput);
+				inputLayout.setVisibility(LinearLayout.VISIBLE);
+				
+				Spinner locationSpinner = (Spinner) findViewById(R.id.spinnerLocation);
+				locationSpinner.setEnabled(true);
+			}
+		});
+		
+		// show a message, when a tag id is long pressed
+		listViewScannedTags.setOnItemLongClickListener(new OnItemLongClickListener()
+		{
+			@Override
+			public boolean onItemLongClick(AdapterView<?> arg0, View arg1, int position, long id)
+			{	
+				Toast infoToast = Toast.makeText(getApplicationContext(), "This is record no. " + position, Toast.LENGTH_SHORT);
+				infoToast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+				infoToast.show();
+				
+				// TODO read out infos to these ids
+				return false;
+			}
+		});
+	}
+	
+	/**
+	 * Start stop button click.
+	 * Changes the button text from start to stop and the other way around.
+	 * Tells the reader to scan.
+	 *
+	 * @param view the view
+	 */
+	public void startStopButtonClick(View view)
+	{
+		Button startStopButton = (Button) findViewById(R.id.buttonStartStopScanning);
+		
+		ListView listViewScannedTags = (ListView) findViewById(R.id.listViewScannedTags);
+		TextView textViewScannedTags = (TextView) findViewById(R.id.textViewScannedTags);
+
+		// change text on click and show scan results
+		if (startStopButton.getText().equals("Start scanning"))
+		{
+			startStopButton.setText("Stop scanning");
+			
+			textViewScannedTags.setVisibility(TextView.VISIBLE);
+			listViewScannedTags.setVisibility(ListView.VISIBLE);
+			listViewScannedTags.setEnabled(false);
+		}
+		else 
+		{	
+			startStopButton.setText("Start scanning");
+			
+			listViewScannedTags.setEnabled(true);
+		}
+		
+		// TODO tell reader to scan
+		// something like:
+		// List<String> scannedTagsList = Reader.scan();
+		
+		// display results in the listview
+		List<String> scannedTagsList = Arrays.asList("000120304000056", "012000040450054" );
+		ListAdapter adapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.scanned_tags_list_element, scannedTagsList);
+		listViewScannedTags.setAdapter(adapter);
+	}
+	
+	/**
+	 * This handles the save button click.
+	 * It will write the results to the Database
+	 *
+	 * @param view the view
+	 */
+	public void saveButtonClick(View view)
+	{
+		ListView listViewScannedTags = (ListView) findViewById(R.id.listViewScannedTags);
+		Spinner spinnerRoom = (Spinner) findViewById(R.id.spinnerRoom);
+		Spinner spinnerEmployee = (Spinner) findViewById(R.id.spinnerEmployee);
+		
+		SparseBooleanArray checked = listViewScannedTags.getCheckedItemPositions();
+		
+		for (int i = 0; i < checked.size(); ++i)
+		{
+			if (checked.get(i) == true)
+			{
+				String rfidId =  (String) listViewScannedTags.getItemAtPosition(i);
+				String selectedRoom = (String) spinnerRoom.getSelectedItem();
+				String selectedEmployee = (String) spinnerEmployee.getSelectedItem();
+				// TODO save to database
+			}
+		}
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu)
 	{
@@ -73,5 +276,4 @@ public class MainActivity extends Activity
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
 }
